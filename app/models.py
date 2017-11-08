cd4ebd4cc6c25109bcc8b49db1b3928e5f952331
benjani8 <ben.m.randerson@gmail.com>
2017-11-08 15:14:02 +0000

    adds api for components

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from flask import url_for
 from . import db
+from .exceptions import ValidationError
 
 
 class Component(db.Model):
@@ -10,3 +12,19 @@ class Component(db.Model):
     
     def __repr__(self):
         return '<Component {}>'.format(self.ident)
+    
+    def get_url(self):
+        return url_for('api.get_component', id=self.id, _external=True)
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'ident': self.tag,
+        }
+
+    def import_data(self, data):
+        try:
+            self.ident = data['ident']
+        except KeyError as e:
+            raise ValidationError('Invalid valve: missing ' + e.args[0])
+        return self
