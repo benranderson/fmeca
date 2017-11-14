e65c643ac4a4d98f21659ac0b33609943b4dda3e
Ben Randerson <ben.m.randerson@gmail.com>
2017-11-14 12:01:26 +0000

    hooks up api error handlers

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import index, facilities, areas, components, subcomponents, failure_modes
+from . import errors, index, facilities, areas, components, subcomponents, failure_modes
