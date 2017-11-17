4818d80654f10ae1c2ce843b3a0bf1bda53e5f64
Stuart Roy <stuart.roy@woodgroup.com>
2017-11-17 14:29:00 +0000

    Reformatted JSON input

--- core/fmeca.py
+++ core/fmeca.py
@@ -2,9 +2,14 @@
 
 class FMECA:
     
-    def __init__(self, subcomponents):
-        self.subcomponents = subcomponents
+    def __init__(self, db_session):
+        self.db_session = db_session
         self._compile_fmeca()
+        self.fmeca_rows = {}
         
     def _compile_fmeca():
+        for sc in self.cubcomponents:
+            
+            for fm in sc.failure_modes:
+            
         pass
